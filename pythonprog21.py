7194460b74385d6b605be7039c9cef36602d221a
necr <necr170400@gmail.com>
2022-07-21 17:18:55 +0400

    initial commit

---
+++ pythonprog21.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3.9
+# pythonprog21
+
+s = str(input())
+a = int(s[0])
+b = int(s[1])
+c = int(s[2])
+d = int(s[3])
+e = int(s[4])
+f = int(s[5])
+if a + b + c == d + e + f:
+    print("Счастливый")
+else:
+    print("Обычный")
+
